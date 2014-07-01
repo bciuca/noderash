e4c5d5ac42f6a765323fae00b2ff93c154ae0a56
Bogdan Ciuca <bogdan.ciuca@gmail.com>
2014-06-30 23:15:16 -0700

    Testing changes with some real switches and a pi.

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@ var Button = require('../rx-gpio/rpreadable'),
     LED = require('../rx-gpio/rpwriteable'),
     utils = require('../rx-gpio/rputils'),
     g2p = utils.gpioToPinMapping,
-    gpio = require('../test/gpio-stub').stub,
+    gpio = utils.getGpioLib(),
     Rx = require('rx');
 
 console.log('testing stub ... ');
 
 var button1 = Button.create(g2p.$17);
-button1.read().subscribe(function(val) {
+button1.changed().subscribe(function(val) {
         console.log('onNext read pin ' + button1._pin + ':', val);
     }, function(e) {
         console.log('onError read pin ' + button1._pin + ':', e, e.stack);
@@ -17,8 +17,8 @@ button1.read().subscribe(function(val) {
         button1.dispose();
     });
 
-var button2= Button.create(g2p.$24);
-button2.changed().delay(3000).take(3).subscribe(function(val) {
+var button2= Button.create(g2p.$18);
+button2.changed().subscribe(function(val) {
         console.log('onNext read pin ' + button2._pin + ':', val);
     }, function(e) {
         console.log('onError read pin ' + button2._pin + ':', e, e.stack);
