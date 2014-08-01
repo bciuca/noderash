2ec9352ee6fba423ed6f7c895ef84d0892f8daf8
Bogdan Ciuca <bogdan.ciuca@gmail.com>
2014-07-31 23:09:50 -0700

    Test harness for switch panel.

--- test/test.js
+++ test/test.js
@@ -1,92 +1,37 @@
+'use strict';
+
 var RPSwitch = require('../rx-gpio/rpswitch'),
     RPToggleSwitch = require('../rx-gpio/rplightedtoggleswitch'),
     RPLED = require('../rx-gpio/rpled'),
     RPBigRedButton = require('../rx-gpio/rpbigredbutton'),
+    RPSwitchPanel = require('../rx-gpio/rpswitchpanel'),
     utils = require('../rx-gpio/rputils'),
     g2p = utils.gpioToPinMapping,
     Rx = require('rx');
 
-console.log('testing stub ... ');
+console.log('Testing stub ...');
 
-var G_KEY   = 11;//g2p.$17;
-var G_TGL   = 15;//g2p.$27;
-var G_BR    = 13;//g2p.$22;
-var G_BRLED = 12;//g2p.$18;
-var G_TGLED = 16;//g2p.$23;
+RPSwitchPanel.initialize({
+    keySwitchAction: function(v) {
+        console.log('key switch', v);
+    },
 
-var keySwitch = RPSwitch.create(G_KEY);
-var toggleSwitch = RPToggleSwitch.create(G_TGL, G_TGLED);
-var bigRedButton = RPBigRedButton.create(G_BR, G_BRLED);
+    toggleSwitchAction: function(v) {
+        console.log('toggle switch', v);
+    },
 
-keySwitch.closed()
-    .map(function() {
-        return toggleSwitch.activate();
-    })
-    .switch()
-    .subscribe(function(val) {
-        console.log('onNext key switch is ON');
-    }, function(e) {
-        console.log('onError key switch read pin', e);
-    }, function() {
-        console.log('onCompleted key switch read pin');
-    });
+    bigRedButtonAction: function() {
+        console.log('BOOOOM!!!');
+    }
+});
 
-keySwitch.open()
-    .flatMap(function() {
-        return toggleSwitch.deactivate();
-    })
-    .flatMap(function() {
-        return bigRedButton.deactivate();
-    })
-    .subscribe(function(val) {
-        console.log('onNext key switch is OFF');
-    }, function(e) {
-        console.log('onError key switch read pin', e);
-    }, function() {
-        console.log('onCompleted key switch read pin');
-    });
-
-toggleSwitch.closed()
-    .flatMap(function() {
-        console.log('activate red button');
-        return bigRedButton.activate();
-    })
-    .subscribe(function(val) {
-        console.log('onNext toggle switch is ON');
-    }, function(e) {
-        console.log('onError toggle switch', e);
-    }, function() {
-        console.log('onCompleted toggle switch read pin');
-    });
-
-toggleSwitch.open()
-    .flatMap(function() {
-        console.log('toggle switch is off');
-        return bigRedButton.deactivate();
-    })
-    .subscribe(function(val) {
-        console.log('onNext toggle switch is OFF');
-    }, function(e) {
-        console.log('onError toggle switch', e);
-    }, function() {
-        console.log('onCompleted toggle switch read pin');
-    });
-
-bigRedButton.up()
-    .subscribe(function() {
-        console.log('Big red button PRESSED');
-    });
-
+// Kill everything here.
 var killMe = function() {
-    toggleSwitch._led.off()
-        .merge(bigRedButton._led.off())
+    RPSwitchPanel.dispose()
         .subscribe(function() {
             process.exit();
         });
 };
 
 process.on('SIGINT', killMe);
-process.on('SIGTERM', function() {
-    console.log('killin it');
-    killMe();
-});
+process.on('SIGTERM', killMe);
