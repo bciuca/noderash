023a755f1cb2f91b0b1c2cb70712f702f35eebe4
Bogdan Ciuca <bogdan.ciuca@gmail.com>
2014-08-07 19:06:22 -0700

    tetsing new onoff stuff

--- test/test.js
+++ test/test.js
@@ -1,19 +1,51 @@
 'use strict';
 
 var RPWriteable = require('../rx-gpio/rpwriteable');
+var Rx = require('rx');
+var RxGpio = require('../rx-gpio/rxgpio');
+var RxLed = require('../rx-gpio/rxled');
+var c = 0;
 
-RPWriteable.create(24).setValue(1).subscribe(function(val) {
-    console.log('next', val);
-},
-
-function(err) {
-    console.log('error', err);
-}, 
-
-function() {
-    console.log('done');
+var led = RxLed.create(24, 1);
+led.off().subscribe(function(v) {
+    console.log('value led=', v);
+}, function(err) {console.log('error', err.stack);}, function() {
+    console.log('led completed');
 });
 
+
+
+var w = RxGpio.createReadable(27, 'both');
+w.watch()
+    .flatMap(function() {
+        return led.toggle();
+    })
+    .subscribe(function(v) {
+        console.log('value=', v);
+        if (c++ > 5) w.destroy();
+    }, function(err) {console.log('error', err.stack);}, function() {
+        console.log('watch completed');
+    });
+
+// var led = RPWriteable.create(24);
+//Rx.Observable.concat(
+//        led.setValue(1),
+//        led.getValue(),
+//        led.setValue(0),
+//        led.getValue()
+//    )
+//    .subscribe(function(val) {
+//        //console.log('next', val);
+//    },
+//
+//    function(err) {
+//        console.log('error', err.stack);
+//    },
+//
+//    function() {
+//        console.log('done');
+//    });
+//
 // var RPSwitchPanel = require('../rx-gpio/rpswitchpanel'),
 //     noop = function() {};
 
